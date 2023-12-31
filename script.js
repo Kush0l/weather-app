51c9db1577bf15e1dd2e9ce08813137a26381c40
Kushal <kush53215321@gmail.com>
2023-12-31 21:33:16 +0530

    Uploded Files

---
+++ script.js
@@ -0,0 +1,47 @@
+url ="https://api.openweathermap.org/data/2.5/weather?q=";
+
+// url ="https://api.openweathermap.org/data/2.5/weather?q= {city name} &appid= {API key} &units=metric";
+
+let apiKey="0a0fbb2d3ac2c356bb408c74d73362ac";
+
+let temp=document.querySelector(".temperature");
+
+let humidityData=document.querySelector(".humidityData p");
+
+let windData=document.querySelector(".windData p");
+
+let weatherType=document.querySelector(".weatherType")
+
+let city;
+
+
+
+let btn=document.querySelector(".search-btn");
+
+btn.addEventListener("click",()=>{
+    city=document.querySelector("#input").value;
+    apiUrl=`${url}${city}&appid=${apiKey}&units=metric`;
+    getWeather();
+})
+
+
+
+
+
+async function getWeather(){
+    response= await fetch(apiUrl);
+    if(response.status == 404){
+        alert("Enter correct city name");
+    }else{
+    data= await response.json();
+    temperature=data.main.temp;
+    humidity=data.main.humidity;
+    windSpeed=data.wind.speed;
+    weatherData=data.weather[0].description;
+    weatherType.innerText=weatherData;
+    temp.innerText =`${temperature}°C`;
+    humidityData.innerText=humidity;
+    windData.innerText=windSpeed;
+    }
+}
+
